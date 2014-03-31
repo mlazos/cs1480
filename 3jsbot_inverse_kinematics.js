7b9ebdfe4deb70798e6253496a87ebeaaa8211d6
Michael Lazos <Mike@Michaels-MacBook-Pro.local>
2014-03-31 17:52:32 -0400

    fixed assignment 4 for real

--- 3jsbot_inverse_kinematics.js
+++ 3jsbot_inverse_kinematics.js
@@ -12,5 +12,23 @@ robot_inverse_kinematics
 iterate_inverse_kinematics
 */
 
+funtction robot_inverse_kinematics(target_pos, endeffector_joint, endeffector_local_pos) {
+    // compute joint angle controls to move location on specified link to Cartesian location
+    if (update_ik) {
+        //iterate_inverse_kinematics(target_pos, endeffector_joint, endeffector_local_pos);
+        for( link in robot.links )
+		{
+			if( link.children == [] )
+				var end_xform = link.xform;
+		}					
 
+		endeffector_geom.visible = true;
+        target_geom.visible = true;
+    }
+    else {
+        endeffector_geom.visible = false;
+        target_geom.visible = false;
+    }
+    update_ik = false;
 
+}
